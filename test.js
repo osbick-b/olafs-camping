dee583af6bd06ed529ae4fbb90adc4383fe203cd
Lucien <osbick.b@gmail.com>
2022-07-19 17:18:11 +0200

    got x-days-from-date thing done with getting the value from HTML custom attribute

--- test.js
+++ test.js
@@ -14,24 +14,24 @@ price.addEventListener("input", function() {
 const controlledDateInput = document.querySelector("#date-today");
 const todayDisplay = document.querySelector("#show-today");
 
-const dateOffsetInput = document.querySelector("#date-in-x-days");
+const dateOffset =
+    +document.querySelector("#date-in-x-days").dataset.xDaysOffset; // ! ---> gotta convert to INT!!!!
 // --- HTML custom attributes ref - https://timnwells.medium.com/custom-attributes-in-html-correctly-681c014a4295
 // --- accessing data: elementName.dataset.customAttributeName
 // --- you have to convert case (it parses automatically. eg. data-x-days-offset ---> xDaysOffset)
-console.log(`dateOffsetInput.xDays`, dateOffsetInput.dataset.xDaysOffset);
-
 
 
 // --- Date offset fn
 const today = new Date();
-function addDays(numDays, date = today) {
+function addDays(numDays = dateOffset, date = today) {
     // console.log(`date`, date);
     date.setDate(date.getDate() + numDays); // ! -- this alters the value of _date_
     // console.log(`date AFTER`, date);
     return date;
 }
 
-console.log(`addDays(7)`, addDays(7));
+// console.log(`addDays(7)`, addDays(7));
+console.log(`addDays()`, addDays());
 
 // controlledDateInput.min = today.toISOString().split("T")[0]; // uses "T" as el separator and picks the 1st el [0];
 
