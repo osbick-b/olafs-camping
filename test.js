aaa7ae6da494c3b0a0c83876a749928ac1b880d9
Lucien <osbick.b@gmail.com>
2022-07-19 16:35:17 +0200

    figured how to add a week to date

--- test.js
+++ test.js
@@ -11,8 +11,17 @@ price.addEventListener("input", function() {
 const controlledDateInput = document.querySelector("#date-today");
 const todayDisplay = document.querySelector("#show-today");
 
-const today = new Date().toISOString().split("T")[0]; // uses "T" as el separator and picks the 1st el [0]
+const today = new Date();
+function addDays(numDays, date = today) {
+    console.log(`today FN`, today);
+    console.log(`date`, date);
+    date.setDate(date.getDate() + numDays); // ! -- this alters the value of _date_
+    console.log(`date AFTER`, date);
+    return date;
+}
 
-controlledDateInput.min = today;
+console.log(`addDays(7)`, addDays(7));
 
-todayDisplay.textContent = today;
+// controlledDateInput.min = today.toISOString().split("T")[0]; // uses "T" as el separator and picks the 1st el [0];
+
+// todayDisplay.textContent = [today, inAWeek];
